83c3ad5cf6decc41e69d78eff5061bda02bd1c1b
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2026-01-16 14:53:14 +0100

    Add initial implementation of the GitHub Insight CLI with a ping command

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .name('gh-insight')
+  .description('A CLI tool to gain insights from GitHub repositories.')
+  .version('1.0.0');
+
+program
+  .command("ping")
+  .description("Check CLI is working")
+  .action(() => {
+    console.log("gh-insight is alive 🚀");
+  });
+  
+program.parse(process.argv);
