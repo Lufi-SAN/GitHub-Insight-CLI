c34751eaee07baf7d8529143c42436e7cef79caf
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2026-01-22 15:30:24 +0100

    Implement authClient function for GitHub authentication and user data retrieval

--- src/api/github/auth.ts
+++ src/api/github/auth.ts
@@ -1,1 +1,24 @@
-import GitHubClient from "./"
+import GitHubClient from "./client.js"
+import type { GitHubUser } from "./types.js"
+import { authEndpoints } from "./endpoints.js"
+
+export async function authClient(PAT?: string) {
+    const client = new GitHubClient(PAT);
+    try {
+        const [responseJSON, expiry, scopes] = await client.request(authEndpoints.getUser)
+        const tokenJSON = JSON.stringify({
+            token: PAT,
+            user: {
+                login: responseJSON.login,
+                id: responseJSON.id,
+                type: responseJSON.type
+            },
+            scopes: scopes,//scopes granted to token
+            created_at: new Date().toISOString(),//creation time
+            expiry: expiry,//expiry of token
+        })
+        return tokenJSON
+    } catch (error) {
+        throw error;
+    }
+}
