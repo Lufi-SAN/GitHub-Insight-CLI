faa5af1d386fe2a88fd2ffbbc5b0f26b43f436eb
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2026-01-22 15:30:19 +0100

    Refactor GitHubClient request method to improve error handling and response structure

--- src/api/github/client.ts
+++ src/api/github/client.ts
@@ -1,5 +1,5 @@
-import type { RequestOptions } from "http";
 import type { GitHubUser } from "./types.js";
+import networkError from "../utils/customErrors.js";
 
 const BASE_URL = "https://api.github.com/user"
 
@@ -14,7 +14,7 @@ export default class GitHubClient {
         }
     }
 
-    async request(endpoint: string): Promise<GitHubUser> {
+    async request(endpoint: string): Promise<[GitHubUser, string | null, string[]]> {
         const url = `${BASE_URL}${endpoint}`;
         
         const response = await fetch(url, {
@@ -24,30 +24,31 @@ export default class GitHubClient {
         const remainingHeader = response.headers.get('x-ratelimit-remaining');
         const resetHeader = response.headers.get('x-ratelimit-reset');
 
+        let rateLimitCode : string  = "undefined";
         if(response.status === 401) {
-            throw new Error("Unauthorized: Invalid or missing token.");
+            throw new networkError("401", "Unauthorized: Invalid or missing token.");
         }
 
         if(response.status === 429 || response.status === 403) {
+            rateLimitCode = (response.status).toString();
             const waitTime = resetHeader ? Math.max(0, parseInt(resetHeader) * 1000 - Date.now()) : 60000;
             await new Promise(resolve => setTimeout(resolve, waitTime));
-
-            
         }
 
         if (!response.ok) {
-            throw new Error(`GitHub API request failed: ${response.status} ${response.statusText}`);
+            throw new networkError((response.status).toString(), `GitHub API request failed: ${response.status} ${response.statusText}`);
         }
 
         if (response.status === 200) {
+            const tokenExpiry = response.headers.get('github-authentication-token-expiration')
+            const scopes = response.headers.get('x-oauth-scopes')?.split(", ") ?? [];
+            if (scopes.length === 0 || !scopes.includes("read:user") || !scopes.includes("public_repo")) {
+                throw new networkError("403", "Forbidden: Token lacks necessary scopes.");
+            }
             const user = await response.json();
-            return user as GitHubUser;
+            return [user as GitHubUser, tokenExpiry, scopes];
         }
 
-        throw new Error("Forbidden: You have hit a rate limit or your token lacks necessary scopes.");
+        throw new networkError(rateLimitCode, "Forbidden: You have hit a rate limit or your token lacks necessary scopes.");
     }
 }
-
-const github = new GitHubClient(PAT);
-{token: PAT}
-github.request('/user', )
